00ecaf492a3ee9ae2b4ce6fcf7ab757c71af68f2
sergkhr <serg.khrechko@gmail.com>
2020-07-18 22:21:55 +0300

    GetData
    файл локально и путьпрописан напрямую, файл есть в папке рядом скодом, измените прямой путь в коде, на нужный у вас (я не умею ставить путь относительно положения проекта)

---
+++ GetDataFun/GetDataFun/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace GetDataFun
+{
+    public class Data
+    {
+        public string[][] d { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = File.ReadAllText(@"D:\XamarinProjects\HSE msp\GetData\GetData\GetData\data.json");
+            Data data = new Data();
+            data = JsonSerializer.Deserialize<Data>(jsonString);
+
+            Console.WriteLine("check");
+            Console.WriteLine(data.d[0][0]);
+            Console.WriteLine("check");
+        }
+    }
+}
