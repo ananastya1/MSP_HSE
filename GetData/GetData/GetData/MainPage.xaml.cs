606aa1da2b954006aaf6a804517bc2e93f7ff996
sergkhr <serg.khrechko@gmail.com>
2020-07-18 17:39:13 +0300

    GetDataFunction

---
+++ GetData/GetData/GetData/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GetData
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public string URL = "https://archi.ru/map/data";
+        public MainPage()
+        {
+            InitializeComponent();
+            GetData();
+        }
+
+        async void GetData()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+
+                    var responce = await client.GetAsync(URL);
+
+                    if (responce.IsSuccessStatusCode)
+                    {
+                        var payload = JsonConvert.SerializeObject(responce);
+                        var content = new StringContent(payload, Encoding.UTF8, @"application/json");
+                        var data = await content.ReadAsStringAsync();
+                        testL.Text = data;
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                catch
+                {
+                    await DisplayAlert("Error!", "err", "OK!");
+                }
+            }
+        }
+    }
+}
